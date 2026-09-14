74227dd0ba9458b8556c62145e456ed25015c2bc
izzy <israelalemu21@gmail.com>
2026-09-14 10:15:33 +0300

    refactor: use familiar syntax to create a list

--- components/ui/tasks/task-list.tsx
+++ components/ui/tasks/task-list.tsx
@@ -22,7 +22,7 @@ export function TaskList({
   if (isLoading) {
     return (
       <div className="space-y-2">
-        {[...Array(3)].map((_, i) => (
+        {[1, 2, 3].map((_, i) => (
           <Skeleton key={i} className="h-[52px] w-full rounded-lg" />
         ))}
       </div>
